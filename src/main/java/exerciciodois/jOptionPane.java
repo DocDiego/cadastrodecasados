268f8bc041d10612e6d69b40c3635e1ddadcbb74
Diego de Oliveira Castro <diegodocdesigner@gmail.com>
2021-10-20 23:36:16 -0300

    versão 1

---
+++ src/main/java/exerciciodois/jOptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciodois;
+
+/**
+ *
+ * @author doc_d
+ */
+class jOptionPane {
+
+    static void ShowMessageDialogue(Object object, String campo_de_Estado_Civil_esta_vazio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
